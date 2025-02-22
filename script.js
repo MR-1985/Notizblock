918882fdd39a6e76cf319c8fb921decacbe7d635
Marco Rößler <118630069+MR-1985@users.noreply.github.com>
2025-02-22 15:21:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
+
 titles = [];
 notes = [];
+doneTitles = [];
 doneNotes = [];
+archivatedTitles = [];
 archivatedNotes = [];
+deletedTitles = [];
 deletedNotes = [];
 
 function hopToNextInput() {
     document.getElementById('input_note').focus();
 }
+function hopToFirstInput() {
+    document.getElementById('input_title').focus();
+}
 
 function saveNote() {
     let givenNoteRef = document.getElementById('input_note');
-    let givenTitleRef = document.getElementById('input_title')
-    let givenNote = givenNoteRef.value;
-    let givenTitle = givenTitleRef.value;
+    let givenTitleRef = document.getElementById('input_title');
+    let givenNote = givenNoteRef.value.trim();
+    let givenTitle = givenTitleRef.value.trim();
     givenTitle = givenTitle.toUpperCase();
     if (givenNote.length < 3) {
-        alert('Ohne Eintrag geht das nicht')
-    } else {
-        notes.push(givenNote);
-        titles.push(givenTitle);
+        alert('Ohne Eintrag geht das nicht');
+        return;
     }
+    if (givenTitle.length < 3) {
+        alert('Ohne Eintrag geht das nicht');
+        return;
+    }
+
+    notes.push(givenNote);
+    titles.push(givenTitle);
+
     renderNoteList()
     givenNoteRef.value = "";
     givenTitleRef.value = "";
@@ -39,63 +52,89 @@ function renderNoteList() {
 
 function moveNoteToDone(iOfNotes) {
 
-    let toMoveNote = notes.splice(iOfNotes, 1);
+    let toMoveNote = notes.splice(iOfNotes, 1)[0];
+    let toMoveTitle = titles.splice(iOfNotes, 1)[0];
     doneNotes.push(toMoveNote);
+    doneTitles.push(toMoveTitle);
+    console.log('Anzahl an Titeln im To Do = ' + notes.length + ' und ' + doneTitles.length + ' im Done, während sich ' + archivatedTitles.length + ' Titel im Archiv befinden.');
+    console.log('Anzahl an Notizen im To Do = ' + titles.length + ' und ' + doneNotes.length + ' im Done, während sich ' + archivatedNotes.length + ' Notizen im Archiv befinden.');
     renderNoteList();
     renderDoneNotes();
 }
 
 function renderDoneNotes() {
+    let doneTitle = document.getElementById('done_titles')
     let doneList = document.getElementById('done_notes');
+    doneTitle.innerHTML = "";
     doneList.innerHTML = "";
 
-    for (let indexDone = 0; indexDone < doneNotes.length; indexDone++) {
-        const doneNote = doneNotes[indexDone];
-        doneList.innerHTML += doneNoteListTemplate(doneNote, indexDone);
+    for (let iOfDone = 0; iOfDone < doneNotes.length; iOfDone++) {
+        const doneTitle = doneTitles[iOfDone];
+        const doneNote = doneNotes[iOfDone];
+        doneList.innerHTML += doneNoteListTemplate(doneTitle, doneNote, iOfDone);
     }
 }
 
-function archivateNote(indexDone) {
-    let toArchivateNote = doneNotes.splice(indexDone, 1)[0];
+function archivateNote(iOfDone) {
+    let toArchivateTitle = doneTitles.splice(iOfDone, 1)[0];
+    let toArchivateNote = doneNotes.splice(iOfDone, 1)[0];
+    archivatedTitles.push(toArchivateTitle);
     archivatedNotes.push(toArchivateNote);
 
+    localStorage.setItem("archivatedTitles", JSON.stringify(archivatedTitles));
     localStorage.setItem("archivatedNotes", JSON.stringify(archivatedNotes));
 
-    archivatedNotes = JSON.parse(localStorage.getItem("archivatedNotes"));
+    console.log('Anzahl an Titel im Archiv = ' + archivatedTitles.length + ', im Done sind es ' + doneTitles.length + ' Titel ' + 'und ' + titles.length + ' Titel im To Do.');
+    console.log('Anzahl an Notizen im Archiv = ' + archivatedNotes.length + ', im Done sind es ' + doneNotes.length + ' Notizen ' + 'und ' + notes.length + ' Notizen im To Do.');
     renderDoneNotes();
     renderArchivatedNotes();
 }
 
 function renderArchivatedNotes() {
+    let archivTitle = document.getElementById('archivated_title')
     let archivList = document.getElementById('archivated_list');
     archivList.innerHTML = "";
+    archivTitle.innerHTML = "";
 
     for (let indexArchivated = 0; indexArchivated < archivatedNotes.length; indexArchivated++) {
+        const archivatedTitle = archivatedTitles[indexArchivated];
         const archivatedNote = archivatedNotes[indexArchivated];
-        archivList.innerHTML += archivatedNoteListTemplate(archivatedNote, indexArchivated);
+        archivList.innerHTML += archivatedNoteListTemplate(archivatedTitle, archivatedNote, indexArchivated);
     }
 }
 function loadNotesFromStorage() {
+    archivatedTitles = JSON.parse(localStorage.getItem("archivatedTitles")) || [];
     archivatedNotes = JSON.parse(localStorage.getItem("archivatedNotes"));
-    renderArchivatedNotes();
+    renderArchivatedNotes() || [];
 }
 
-function moveNoteUpToToDo(archivatedNote, indexArchivated) {
+function moveNoteUpToToDo(indexArchivated) {
+    let movedUpTitle = archivatedTitles.splice(indexArchivated, 1);
+    titles.push(movedUpTitle);
     let movedUpNote = archivatedNotes.splice(indexArchivated, 1);
     notes.push(movedUpNote);
+    console.log('Anzahl an Titeln im To Do = ' + titles.length + ', im Done sind ' + doneTitles.length + ' und im Archiv ' + archivatedTitles.length + '.');
+    console.log('Anzahl an Notizen im To Do = ' + titles.length + ', im Done sind ' + doneNotes.length + ' und im Archiv ' + archivatedNotes.length + '.');
     renderArchivatedNotes();
     renderNoteList();
 }
 
 function deleteIt(indexArchivated) {
-    let deleted = archivatedNotes.splice(indexArchivated, 1);
-    deletedNotes.push(deleted);
+    let deletedTitle = archivatedTitles.splice(indexArchivated, 1)[0];
+    deletedTitles.push(deletedTitle);
+    localStorage.setItem("archivatedTitles", JSON.stringify(archivatedTitles));
+    if (deletedTitles.length > 0) {
+        deletedTitles.length = 0;
+    }
+
+    let deletedNote = archivatedNotes.splice(indexArchivated, 1);
+    deletedNotes.push(deletedNote);
     localStorage.setItem("archivatedNotes", JSON.stringify(archivatedNotes));
     if (deletedNotes.length > 0) {
         deletedNotes.length = 0;
-        console.log('Keine gespeicherten Notizen mehr in der Liste')
     }
-    console.log('Anzahl an Notizen = ' + deletedNotes.length);
+    console.log('Anzahl an Titeln im Archiv = ' + archivatedNotes.length + ' im Done sind es ' + doneTitles.length + ' Titel ' + 'und ' + titles.length + ' Titel im To Do.');
+    console.log('Anzahl an Notizen im Archiv = ' + archivatedNotes.length + ', im Done sind es ' + doneNotes.length + ' Notizen ' + 'und ' + notes.length + ' Notizen im To Do.');
     renderArchivatedNotes();
     renderNoteList();
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,12 @@
 function noteListTemplate(title, note, iOfNotes) {
-    return `<h4 class="title_color"> ${title}</h4><li class="li"> ${note}<button class="button to_done" onclick="moveNoteToDone(${note, iOfNotes})">✓</button></li>`
+    return `<h4 class="title_color"> ${title}</h4><li class="li"> ${note}<button class="button to_done" onclick="moveNoteToDone(${title, note, iOfNotes})">✓</button></li>`
 }
 
-function doneNoteListTemplate(doneNote, indexDone) {
-    return `<li class="moved_li">${doneNote}<button class="button archivate" onclick="archivateNote(${doneNote, indexDone})">A</button></li>`
+function doneNoteListTemplate(doneTitle, doneNote, indexDone) {
+    return `<h4 class="title_color"> ${doneTitle}</h4><li class="moved_li">${doneNote}<button class="button archivate" onclick="archivateNote(${doneNote, indexDone})">A</button></li>`
 }
 
-function archivatedNoteListTemplate(archivatedNote, indexArchivated) {
-    return `<li class="deleted_li">${archivatedNote}<button class="button back_to_notes" onclick="moveNoteUpToToDo(${archivatedNote, indexArchivated})">N</button><button class="button delete" onclick="deleteIt(${archivatedNote, indexArchivated})">D</button></li>`
+function archivatedNoteListTemplate(archivatedTitle, archivatedNote, indexArchivated) {
+    return `<h4 class="title_color"> ${archivatedTitle}</h4><li class="deleted_li">${archivatedNote}<button class="button back_to_notes" onclick="moveNoteUpToToDo(${archivatedNote, indexArchivated})">N</button><button class="button delete" onclick="deleteIt(${archivatedNote, indexArchivated})">D</button></li>`
 }
 
